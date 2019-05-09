2786d3b4a8027330913641e61eb2039056167897
ghazi200 <techworldstarzllc@gmail.com>
2019-05-09 02:36:45 -0400

    Add files via upload

---
+++ listhw.py
@@ -0,0 +1,15 @@
+# Checking of element existence
+
+# using loops and in
+
+checklist=[ 1,4,5,9,7,2,3]
+
+print("Checking to see if 3 exists in the list")
+
+for i in checklist:
+    if(i==3):
+        print ("The Element Exists")
+
+
+
+    
